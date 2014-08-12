3328ef466cb4d42beee2e1c5172cf69b39cce9f0
Bojan Kraut <bojan.kraut@alcyone.si>
2014-08-12 16:31:37 +0200

    ARActivity

---
+++ Aravet/src/org/ldv/aravet/ar/electonics/ElectronicsActivity.java
@@ -0,0 +1,18 @@
+package org.ldv.aravet.ar.electonics;
+
+import org.ldv.aravet.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ElectronicsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.electronics);
+		
+		
+	}
+	
+}
