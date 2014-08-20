6513efd11a447b4057182e06c5d7184122d06a22
Bojan Kraut <bojan.kraut@alcyone.si>
2014-08-20 17:00:16 +0200

    Again

---
+++ Aravet/src/org/ldv/aravet/ar/clothes/ClothesActivity.java
@@ -0,0 +1,17 @@
+package org.ldv.aravet.ar.clothes;
+
+import org.ldv.aravet.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClothesActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.clothes);
+
+	}
+	
+}
---
+++ Aravet/src/org/ldv/aravet/ar/computer/ComputerActivity.java
@@ -0,0 +1,18 @@
+package org.ldv.aravet.ar.computer;
+
+import org.ldv.aravet.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ComputerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.computer);
+		
+		
+	}
+	
+}
